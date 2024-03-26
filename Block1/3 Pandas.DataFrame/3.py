41f39bceaf101dfd5b495ee8cfd4982bcd138d9e
umkasort <mironkina@mail.ru>
2024-03-26 18:42:05 +0300

    1

--- Block1/3 Pandas.DataFrame/3.py
+++ Block1/3 Pandas.DataFrame/3.py
@@ -1,11 +1,4 @@
-import pandas as pd
-def create_companyDF(income, expenses, years):
-    res=pd.DataFrame({'Income':income,'Expenses':expenses})
-    res.index=years
-    return res
-def get_profit(df, year):
-    res=df.iloc[[year],['income']]
-    return res
-
-print (df = create_companyDF([612, 516, 329, 158], [136,163,250,361], [2017,2018,2019,2020]))
- print(get_profit(year = 2018, df = create_companyDF([612, 516, 329, 158], [136,163,250,361], [2017,2018,2019,2020])))
+## Введите свое решение ниже
+prices = {'Adidas': 4298, 'Nike': 6550, 'Puma': 4490, 'Asics': 3879}
+purchases = ['Adidas', 'Nike']
+print(prices[purchases[1]])
